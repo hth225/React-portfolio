ec9f30301331a3c34252fac773086e12d9e9b917
hth225 <hth225@naver.com>
2018-08-31 11:13:33 +0900

    sidebar example codes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
-import React, { Component } from 'react';
-import Header from './components/Header'
+// import React, { Component } from 'react';
+// import Header from './components/Header'
 
-class App extends Component {
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <Header />
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+import React from "react";
+import Sidebar from "react-sidebar";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sidebarOpen: true
+    };
+    this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
+  }
+
+  onSetSidebarOpen(open) {
+    this.setState({ sidebarOpen: open });
+  }
+
   render() {
     return (
-      <div className="App">
-        <Header />
-      </div>
+      <Sidebar
+        sidebar={
+          <div>
+            <b>Sidebar content</b>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+          </div>
+        }
+        open={this.state.sidebarOpen}
+        onSetOpen={this.onSetSidebarOpen}
+        styles={{ sidebar: { background: "powderblue" } }}
+      >
+        <button onClick={() => this.onSetSidebarOpen(true)}>
+          Open sidebar
+        </button>
+      </Sidebar>
     );
   }
 }
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,42 +1,45 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import SideBar from "react-sidebar";
 
+const mql = window.matchMedia(`(min-width: 800px)`);
 class SideBar extends Component {
-    state = {
-        isBarOpen: false,
-    }
-    onItemClicked() {
-        this.setState({isBarOpen: false})
-    }
-    onSideBarClicked() {
-        this.setState({isBarOpen: !this.state.isBarOpen})
-    }
-    
-    render() {
-        const NavItem = [
-            {
-                title: "Top",
-                url: "#top"
-            },
-            {
-                title: "About",
-                url: "#about"
-            },
-            {
-                title: "Portfolio",
-                url: "#portfolio"
-            },
-            {
-                title: "Contact",
-                url: "#contact"
-            },
-        ]
-        return (
-            <div>
-                
-            </div>
-        );
-    }
-    
+  state = {
+    isBarOpen: false,
+    sidebarDocked: mql.matches
+  };
+  onItemClicked() {
+    this.setState({ isBarOpen: false });
+  }
+  onSideBarClicked() {
+    this.setState({ isBarOpen: !this.state.isBarOpen });
+  }
+  componentWillMount() {
+
+      mql.addListener(this.mediaQueryChanged);
+  }
+  
+
+  render() {
+    const NavItem = [
+      {
+        title: "Top",
+        url: "#top"
+      },
+      {
+        title: "About",
+        url: "#about"
+      },
+      {
+        title: "Portfolio",
+        url: "#portfolio"
+      },
+      {
+        title: "Contact",
+        url: "#contact"
+      }
+    ];
+    return <div />;
+  }
 }
 
 export default SideBar;
